b9d82885920ca82e262dc16ff712f316f65f3022
Medina-Leguizamo <erwinmedina@csu.fullerton.edu>
2024-04-14 19:58:54 -0700

    Added the .env portion and adjusted app.py

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from pymongo import MongoClient
 
 # Correct the Blueprint import based on your file and folder structure
 from routes.routes import main_routes  # If main_routes is defined in routes/routes.py
 
 def create_app():
+
+    # Handles .env file read.
+    load_dotenv()
+    mongo_uri = os.environ.get('MONGODB_URI')
+    mongo_client = MongoClient(mongo_uri)
+    db = mongo_client["PetStoreProject"]
+    collection = db["PetStore"]
+
     app = Flask(__name__)
 
     # Configuration
-    app.config['MONGO_URI'] = "mongodb://localhost:27017/petstore"
-    app.config['SECRET_KEY'] = "your_secret_key_here"
-    app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this in production
+    # app.config['MONGO_URI'] = mongo_uri
+    # app.config['SECRET_KEY'] = "your_secret_key_here"
+    # app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this in production
 
     # Initialize MongoDB
-    mongo = PyMongo(app)
+    data = collection.find_one()
+    print(data)
 
     # Initialize bcrypt and JWT
     bcrypt = Bcrypt(app)
