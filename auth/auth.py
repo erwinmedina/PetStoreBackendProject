647bbeb566c23673b5dfc269e6119f4093791937
Medina-Leguizamo <erwinmedina@csu.fullerton.edu>
2024-04-21 21:41:56 -0700

    Fixed the create_user issue in auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -4,12 +4,23 @@ from datetime import timedelta
 from models.user import User
 import redis
 import os
+from flask import Blueprint
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
 # Setup Redis connection
 redis_client = redis.StrictRedis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), password=os.getenv('REDIS_PASSWORD'), decode_responses=True)
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
+
+userapp = Blueprint('userapp', __name__)
+
+# Handles loading the database
+load_dotenv()
+mongo_uri = os.environ.get('MONGO_URI')
+mongo_client = MongoClient(mongo_uri)
+db = mongo_client["PetStoreProject"]
 
 def initialize_auth(app):
     # Initialize bcrypt and JWT with the Flask app
@@ -19,9 +30,10 @@ def initialize_auth(app):
 def create_user(username, password):
     # Hash password and create a new user in the database
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-    if User.find_by_username(username) is not None:
+
+    if User.find_by_username(username, db) is not None:
         return False, "User already exists"  # User already exists
-    user = User(username=username, password=hashed_password)
+    user = User(username=username, password=hashed_password, db=db)
     success = user.save()
     if success:
         return True, "User created successfully"
@@ -30,7 +42,7 @@ def create_user(username, password):
 
 def authenticate_user(username, password):
     # Check user credentials and return a JWT token if valid
-    user = User.find_by_username(username)
+    user = User.find_by_username(username, db)
     if not user:
         return False, "User not found", None
     if bcrypt.check_password_hash(user.password, password):
